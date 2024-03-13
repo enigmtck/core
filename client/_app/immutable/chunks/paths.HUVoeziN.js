f4645916262b3c2ac4e92912b03438694dfd8c08
Justin Thomas <justin@jdt.io>
2024-03-12 20:26:58 -0700

    hide some unbuilt features

--- client/_app/immutable/chunks/paths.HUVoeziN.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_pqbmdu?.base??"",a=globalThis.__sveltekit_pqbmdu?.assets??s;export{a,s as b};
---
+++ client/_app/immutable/chunks/paths.nOySfiN5.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_a46bm9?.base??"",a=globalThis.__sveltekit_a46bm9?.assets??s;export{a,s as b};
