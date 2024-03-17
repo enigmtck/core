82a46359197a9a0c3f8c7316d3d1afd206c6a31d
Justin Thomas <justin@jdt.io>
2024-03-16 22:16:56 -0700

    upgrade client

--- client/_app/immutable/chunks/paths.4Fk-lbdf.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_c9w63a?.base??"",a=globalThis.__sveltekit_c9w63a?.assets??s;export{a,s as b};
---
+++ client/_app/immutable/chunks/paths.P4-Op3ns.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1qzgzn5?.base??"",a=globalThis.__sveltekit_1qzgzn5?.assets??s;export{a,s as b};
