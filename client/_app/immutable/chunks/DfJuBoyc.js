d984fccf5b55e4cfc074eb7f792177a2f8282065
Justin Thomas <justin@jdt.io>
2025-11-25 20:49:18 -0800

    Release v0.5.6

---
+++ client/_app/immutable/chunks/DfJuBoyc.js
@@ -0,0 +1,1 @@
+import{s as e}from"./2w9f54dl.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
---
+++ client/_app/immutable/entry/start.DZX72YL8.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/2w9f54dl.js";import{w as t}from"../chunks/Dgy-dwZ8.js";export{t as load_css,r as start};
